1d46eb6dbf0c0a49248cf1c4a05bb487574737a2
Vinícius Yoda <vyoda4604@gmail.com>
2023-01-10 17:44:39 -0300

    arrumando a pasta de imagens

--- src/componentes/Banner/Banner.js
+++ src/componentes/Banner/Banner.js
@@ -3,7 +3,7 @@ import './Banner.css'
 export const Banner = () => {
    return (
       <header className="banner">
-         <img src="../imagens/banner.jpg" alt="O banner principal da página do Organo"/>
+         <img src="/imagens/banner.png" alt="O banner principal da página do Organo"/>
       </header>
       
    )
