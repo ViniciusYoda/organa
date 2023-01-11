3c6c380b1424a81d04cb863eb8c83b37125dd150
Vinícius Yoda <vyoda4604@gmail.com>
2023-01-11 17:05:43 -0300

    terminando o formulario

--- src/componentes/Formulario/index.js
+++ src/componentes/Formulario/index.js
@@ -1,14 +1,36 @@
 import CampoTexto from '../CampoTexto'
+import ListaSuspensa from '../ListaSuspensa'
+import Botao from '../Botao'
 import './Formulario.css'
 
 const Formulario = () => {
+
+   const times = [
+      'Programação',
+      'Front-End',
+      'Data Science',
+      'DevOps',
+      'UX e Design',
+      'Mobile', 
+      'Inovação e Gestão'
+   ]
+
+   const aoSalvar = (evento) => {
+      evento.preventDefault()
+      console.log('From foi submetido')
+   }
+
    return (
       <section className='formulario'>
-         <form>
+         <form onSubmit={aoSalvar}>
             <h2>Preencha os dados para criar o card do colaborador</h2>
-            <CampoTexto label="Nome" placeholder="Digite seu nome"/>
-            <CampoTexto label="Cargo" placeholder="Digite o seu Cargo"/>
-            <CampoTexto label="Imagem" placeholder="Digite o endereço da imagem"/>
+            <CampoTexto obrigatorio={true} label="Nome" placeholder="Digite seu nome"/>
+            <CampoTexto obrigatorio={true} label="Cargo" placeholder="Digite o seu Cargo"/> 
+            <CampoTexto obrigatorio={true} label="Imagem" placeholder="Digite o endereço da imagem"/>
+            <ListaSuspensa obrigatorio={true} label="time" itens={times} />
+            <Botao>
+               Criar Card
+            </Botao>
          </form>
       </section>
    )
